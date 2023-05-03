096f76453a688c776418dfa000693b15f87905be
Hao Peng <haop@allenai.org>
2023-05-03 05:32:59 +0000

    support raft with t5. mbart bug fix

--- mbart.py
+++ mbart.py
@@ -11,7 +11,7 @@ from transformers import MBartForConditionalGeneration, MBartTokenizer
 class MBART():
     def __init__(self):
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        self.model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-en-ro").to(device)
+        self.model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-en-ro").half().to(device)
         self.tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-en-ro", src_lang="en_XX")
 
         # TODO
@@ -22,12 +22,10 @@ class MBART():
         inputs = self.tokenizer.batch_encode_plus(
             inputs,
             padding=True,
-            truncation="only_first",
             return_tensors="pt",
-            pad_to_multiple_of=8,
         ).input_ids
         inputs = inputs.to(self.model.device)
-        outputs = self.model.generate(inputs, max_length=10)
+        outputs = self.model.generate(inputs)
         outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         for output in outputs:
             yield output.strip()
