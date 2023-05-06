c279202089fd4754bdf613b0a9ad3036638515f5
Jared Fernandez <jaredfern@cmu.edu>
2023-05-05 22:55:19 -0400

    ONNX MBART verified

--- mbart.py
+++ mbart.py
@@ -1,10 +1,12 @@
 import json
+import logging
 import sys
 import argparse
 
 import torch
 import transformers
-from transformers import MBartForConditionalGeneration, MBartTokenizer
+from optimum.onnxruntime import ORTModelForSeq2SeqLM
+from transformers import MBartForConditionalGeneration, MBartTokenizer, pipeline
 
 
 # Submission
@@ -13,39 +15,39 @@ class MBART():
             self,
             pretrained_model_name_or_path='facebook/mbart-large-en-ro',
             use_onnx=False):
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-        self.tokenizer = MBartTokenizer.from_pretrained(
-            pretrained_model_name_or_path, src_lang="en_XX")
+        self.tokenizer = MBartTokenizer.from_pretrained(pretrained_model_name_or_path, src_lang="en_XX")
 
         self.use_onnx = use_onnx
         if self.use_onnx:
             # TODO: Setup ORT Quantizer and Optimizer
             self.onnx_model = ORTModelForSeq2SeqLM.from_pretrained(
-                pretrained_model_name_or_path).to(device)
-            self.onnx_pipeline = pipeline("text2text-generation", model=self.onnx_model, tokenizer=tknr)
-        else
+                pretrained_model_name_or_path).to(self.device)
+            self.onnx_pipeline = pipeline(
+                "text2text-generation", model=self.onnx_model, tokenizer=self.tokenizer, device=0)
+        else:
             self.model = MBartForConditionalGeneration.from_pretrained(
-                pretrained_model_name_or_path).to(device)
+                pretrained_model_name_or_path).to(self.device)
 
         # TODO
         #self._convert_fn = lambda text: text["input"]
 
     def predict(self, inputs):
         # inputs = [self._convert_fn(i) for i in inputs]
-        inputs = self.tokenizer.batch_encode_plus(
-            inputs,
-            padding=True,
-            truncation="only_first",
-            return_tensors="pt",
-            pad_to_multiple_of=8,
-        ).input_ids
-        inputs = inputs.to(self.model.device)
         if self.use_onnx:
-            outputs = pipeline(inputs)
+            outputs = self.onnx_pipeline(inputs)
             for output in outputs:
                 yield output["generated_text"]
         else:
+            inputs = self.tokenizer.batch_encode_plus(
+                inputs,
+                padding=True,
+                truncation="only_first",
+                return_tensors="pt",
+                pad_to_multiple_of=8,
+            ).input_ids
+            inputs = inputs.to(self.device)
             outputs = self.model.generate(inputs, max_length=10)
             outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
             for output in outputs:
