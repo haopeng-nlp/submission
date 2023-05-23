0279c99531c47cfe1808cbb2fd33f7d4d635bc8f
Hao Peng <haop@allenai.org>
2023-05-23 20:09:07 +0000

    offline

--- entrypoint.py
+++ entrypoint.py
@@ -1,11 +1,14 @@
 import argparse
 import json
+import os
 import sys
 
 import transformers
+from datasets import Dataset
+
+from example_stdio_submission_sst import GoodBinarySentimentClassifier
 from mbart import MBART
 from t5 import T5
-from example_stdio_submission_sst import GoodBinarySentimentClassifier
 
 
 # We provide this
@@ -23,11 +26,35 @@ def stdio_predictor_wrapper(predictor):
         # Participants need to connect their inference code to our wrapper through the following line.
         outputs = predictor.predict(inputs=inputs)
         # Writes are \n deliminated, so adding \n is essential to separate this write from the next loop iteration.
-        outputs = [o for o in outputs]
+        # outputs = [o for o in outputs]
+        outputs = list(outputs)
         sys.stdout.write(f"{json.dumps(outputs)}\n")
         # Writes to stdout are buffered. The flush ensures the output is immediately sent through the pipe
         # instead of buffered.
         sys.stdout.flush()
+
+
+def offline_predictor_wrapper(predictor: MBART):
+    configs = sys.stdin.readline().rstrip()
+    configs = json.loads(configs)
+    assert isinstance(configs, dict)
+
+    offline_dataset = Dataset.from_json(configs["offline_data_path"])
+    offline_dataset_inputs = [instance["input"] for instance in offline_dataset]
+    predictor.prepare()
+    sys.stdout.write("Model and data loaded. Start the timer.\n")
+    sys.stdout.flush()
+    if configs.get("limit", -1) > 0:
+        offline_dataset_inputs = offline_dataset_inputs[:configs["limit"]]
+    outputs = predictor.predict_offline(offline_dataset_inputs)
+    outputs = list(outputs)
+    sys.stdout.write("Offiline prediction done. Stop the timer.\n")
+    sys.stdout.flush()
+
+    outputs = Dataset.from_list([{"output": o} for o in outputs])
+    outputs.to_json(configs["offline_output_path"])
+    sys.stdout.write("Offiline outputs written. Exit.\n")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
@@ -37,8 +64,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str)
     parser.add_argument("--task", type=str)
+    parser.add_argument("--offline", action="store_true")
     args = parser.parse_args()
-
     if "t5" in args.model:
         predictor = T5(
             pretrained_model_name_or_path=args.model,
@@ -50,4 +77,7 @@ if __name__ == "__main__":
         predictor = GoodBinarySentimentClassifier()
     else:
         raise NotImplementedError()
-    stdio_predictor_wrapper(predictor)
+    if args.offline:
+        offline_predictor_wrapper(predictor)
+    else:   
+        stdio_predictor_wrapper(predictor)
--- mbart.py
+++ mbart.py
@@ -1,8 +1,9 @@
 import json
 import sys
 import argparse
-
+import more_itertools
 import torch
+from typing import List
 import transformers
 from transformers import MBartForConditionalGeneration, MBartTokenizer
 
@@ -10,15 +11,14 @@ from transformers import MBartForConditionalGeneration, MBartTokenizer
 # Submission
 class MBART():
     def __init__(self):
+        self.prepare()
+
+    def prepare(self):
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-en-ro").half().to(device)
         self.tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-en-ro", src_lang="en_XX")
 
-        # TODO
-        #self._convert_fn = lambda text: text["input"]
-
-    def predict(self, inputs):
-        # inputs = [self._convert_fn(i) for i in inputs]
+    def predict(self, inputs: List[str]):
         inputs = self.tokenizer.batch_encode_plus(
             inputs,
             padding=True,
@@ -29,3 +29,18 @@ class MBART():
         outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         for output in outputs:
             yield output.strip()
+
+    def predict_offline(self, inputs: List[str]):
+        batches = more_itertools.chunked(inputs, 32)
+        # inputs = [self._convert_fn(i) for i in inputs]
+        for batch in batches:
+            inputs = self.tokenizer.batch_encode_plus(
+                batch,
+                padding=True,
+                return_tensors="pt",
+            ).input_ids
+            inputs = inputs.to(self.model.device)
+            outputs = self.model.generate(inputs)
+            outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            for output in outputs:
+                yield output.strip()
