57efac2ccaf3f5d655899a4bf53ab7eabc80e8c4
Tom Sherborne <toms@allenai.org>
2023-05-30 13:21:27 -0700

    Refactor to remove arguments to prepare()

--- mbart.py
+++ mbart.py
@@ -1,7 +1,15 @@
 from typing import List, Tuple
 import more_itertools
 import torch
+<<<<<<< HEAD
 from transformers import AutoTokenizer
+=======
+from typing import List, Tuple
+from functools import partial
+import transformers
+from transformers import PreTrainedModel, AutoTokenizer
+from tokenizers import Tokenizer
+>>>>>>> 8ad5125 (Refactor to remove arguments to prepare())
 from transformers import MBartForConditionalGeneration, MBartTokenizer
 from transformers import MBart50Tokenizer
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
@@ -68,6 +76,32 @@ def model_task_to_src_tgt_lang(model: str, task: str) -> Tuple[str]:
 
     return src_lang, tgt_lang
 
+def model_task_to_src_tgt_lang(model: str, task: str) -> Tuple(str, str):
+    """
+    Input a model name and the relevant task and returns the source and target language IDs.
+    Use TASK2SRCTGT to get the 5 char codes. Trim to 2 char if the model is in M2M100_MODELS.
+    Also raises an exception if the combination of model and src_lang, tgt_lang is not valid
+    (i.e., the model is not designed for this translation pair)
+    """
+    src_lang = TASK2SRCTGT.get(task)['src_lang']
+    tgt_lang = TASK2SRCTGT.get(task)['tgt_lang']
+    
+    if model in MBART_MODELS:
+        assert src_lang == "en_XX", f"Model {model} does not support src_lang: {src_lang}"
+        assert tgt_lang == "ro_RO", f"Model {model} does not support tgt_lang: {tgt_lang}"
+
+    if (model == MBART50_MODELS[0]) or (model == M2M100_MODELS[5]): # One to many
+        assert src_lang == "en_XX", f"Model {model} does not support src_lang: {src_lang}"
+
+    if (model == MBART50_MODELS[1]) or (model == M2M100_MODELS[6]): # Many to one
+        assert tgt_lang == "en_XX",  f"Model {model} does not support tgt_lang: {tgt_lang}"
+
+    if model in M2M100_MODELS:
+        src_lang = src_lang[:2]
+        tgt_lang = tgt_lang[:2]
+
+    return src_lang, tgt_lang
+
 # Submission
 class MBART():
     """
@@ -88,14 +122,27 @@ class MBART():
         self._task = task
         self._quantize_mode = quantize_mode
         self.prepare()
+<<<<<<< HEAD
+=======
+
+    def prepare(self) -> None:
+>>>>>>> 8ad5125 (Refactor to remove arguments to prepare())
 
     def prepare(self) -> None:
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+<<<<<<< HEAD
         self.src_lang, self.tgt_lang = model_task_to_src_tgt_lang(model=self._pretrained_model_name_or_path,
                                                                   task=self._task)
         
         self.tokenizer = AutoTokenizer.from_pretrained(self._pretrained_model_name_or_path, 
             src_lang=self.src_lang, tgt_lang=self.tgt_lang) # This is redundant for setting up src and tgt
+=======
+
+        self.src_lang, self.tgt_lang = model_task_to_src_tgt_lang(model=self._pretrained_model_name_or_path,
+                                                                  task=self._task)
+        self.tokenizer = AutoTokenizer.from_pretrained(self._pretrained_model_name_or_path, 
+            src_lang=self.src_lang, tgt_lang=self.tgt_lang) # This is redundant insurance for setting up src and tgt
+>>>>>>> 8ad5125 (Refactor to remove arguments to prepare())
         
         if self._pretrained_model_name_or_path in M2M100_MODELS:
             self.src_lang_id = self.tokenizer.get_lang_id(self.src_lang)
@@ -105,6 +152,7 @@ class MBART():
             self.tgt_lang_id = self.tokenizer.lang_code_to_id[self.tgt_lang]
 
         if self._pretrained_model_name_or_path in MBART_MODELS:
+<<<<<<< HEAD
             _, model_cls, add_arg_key = MBART_PIPELINE
         elif self._pretrained_model_name_or_path in MBART50_MODELS:
             _, model_cls,  add_arg_key = MBART50_PIPELINE
@@ -146,6 +194,40 @@ class MBART():
             print("No model weight quantization selected. Loading in full-precision")
             self.model = MBartForConditionalGeneration.from_pretrained(self.pretrained_model_name_or_path).to(device)
 
+=======
+            _, model_cls = MBART_PIPELINE
+            self.additional_args = {"decoder_start_token_id": self.tgt_lang_id}
+        elif self._pretrained_model_name_or_path in MBART50_MODELS:
+            _, model_cls = MBART50_PIPELINE
+            self.additional_args = {"forced_bos_token_id": self.tgt_lang_id}
+        elif self._pretrained_model_name_or_path in M2M100_MODELS:
+            _, model_cls = M2M100_PIPELINE
+            self.additional_args = {"forced_bos_token_id": self.tgt_lang_id}
+        else:
+            raise ValueError(f"Argument {self._pretrained_model_name_or_path} not recognized!")
+
+        if self._quantize_mode == "fp16":
+            sys.stdout.write("Declaring fp16 precision model")
+            self.model = model_cls.from_pretrained(self._pretrained_model_name_or_path, torch_dtype=torch.float16).to(device)
+        elif self._quantize_mode == "bf16":
+            sys.stdout.write("Declaring bf16 precision model")
+            self.model = model_cls.from_pretrained(self._pretrained_model_name_or_path, torch_dtype=torch.bfloat16).to(device)
+        elif self._quantize_mode == "bb8":
+            sys.stdout.write("Declaring 8-bit precision model")
+            self.model = model_cls.from_pretrained(self._pretrained_model_name_or_path, device_map="auto", load_in_8bit=True)
+            sys.stdout.write(f"8-bit Model is on GPU? {self.model.device==device}")
+        elif self._quantize_mode == "bb4":
+            sys.stdout.write("Declaring 4-bit precision model")
+            self.model = model_cls.from_pretrained(self._pretrained_model_name_or_path, device_map="auto", load_in_4bit=True)
+            sys.stdout.write(f"4-bit Model is on GPU? {self.model.device==device}")
+        else:
+            sys.stdout.write("No model weight quantization selected. Loading in full-precision")
+            self.model = model_cls.from_pretrained(self._pretrained_model_name_or_path).to(device)
+            
+        self.model.eval()
+        sys.stdout.flush()
+        
+>>>>>>> 8ad5125 (Refactor to remove arguments to prepare())
     def predict(self, inputs: List[str]):
         inputs = self.tokenizer.batch_encode_plus(
             inputs,
