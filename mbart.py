fade1edbda3daf5b7c30a412711c3cad856d05dc
Jared Fernandez <jaredfern@cmu.edu>
2023-06-12 15:20:18 -0400

    cleanup onnx mbart

--- mbart.py
+++ mbart.py
@@ -160,13 +160,14 @@ class MBART(AutoSeq2SeqModelSubmission):
 
         self.additional_args = {add_arg_key: self.tgt_lang_id}
         if self._use_onnx:
+            # Manual download because `.from_pretrained()` fails to download of encoder data file
             hf_hub_download(
                 repo_id=ONNX_MODEL_DIR,
                 filename=f"{self._pretrained_model_name_or_path}/encoder_model.onnx.data")
-            self.onnx_model = ORTModelForSeq2SeqLM.from_pretrained(
+            self.model = ORTModelForSeq2SeqLM.from_pretrained(
                 ONNX_MODEL_DIR,
                 subfolder=self._pretrained_model_name_or_path,
-                use_io_binding=torch.cuda.is_available()
+                use_io_binding=torch.cuda.is_available(),
             ).to(device)
 
         if self._quantize_mode == "fp16":
@@ -203,8 +204,8 @@ class MBART(AutoSeq2SeqModelSubmission):
             padding=True,
             return_tensors="pt",
         ).input_ids
-        inputs = inputs.to(self.onnx_model.device)
-        outputs = self.onnx_model.generate(inputs, **self.additional_args)
+        inputs = inputs.to(self.model.device)
+        outputs = self.model.generate(inputs, max_length=32, **self.additional_args)
         outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         for output in outputs:
             yield output.strip()
@@ -220,8 +221,8 @@ class MBART(AutoSeq2SeqModelSubmission):
                 truncation="only_first",
                 pad_to_multiple_of=8,
             ).input_ids
-            inputs = inputs.to(self.onnx_model.device)
-            outputs = self.onnx_model.generate(inputs, max_length=32, **self.additional_args)
+            inputs = inputs.to(self.model.device)
+            outputs = self.model.generate(inputs, max_length=32, **self.additional_args)
             outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
             for output in outputs:
                 yield output.strip()
