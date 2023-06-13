e020e27dcfb66737c0ca564e2f17ce735417a47f
Jared Fernandez <jaredfern@cmu.edu>
2023-06-13 00:39:52 -0400

    onnx exporter downgrade

--- onnx_exporter.sh
+++ onnx_exporter.sh
@@ -4,13 +4,22 @@
 # Usage: ./onnx_exporter.sh
 
 # Name of model to be exported on Huggingface ModelHub
-OUTPUT_DIR="onnx_models";
-HF_MODEL_NAME="facebook/mbart-large-50-many-to-one-mmt"
+OUTPUT_DIR="/data/jaredfer/efficiency-benchmark-onnx"
 
-optimum-cli export onnx \
-    --model $HF_MODEL_NAME \
-    --task text2text-generation-with-past \
-    --framework pt \
-    --device cuda \
-    --optimize O4 \
-    $OUTPUT_DIR/$HF_MODEL_NAME;
+for HF_MODEL_NAME in \
+    "facebook/wmt21-dense-24-wide-x-en" \
+    "facebook/m2m100-12B-avg-5-ckpt" \
+    "facebook/mbart-large-50-many-to-many-mmt" \
+    "facebook/mbart-large-50-many-to-one-mmt" \
+    "Helsinki-NLP/opus-mt-de-en" \
+    "facebook/m2m100_1.2B" \
+    "facebook/m2m100_418M"
+do
+    echo "EXPORTING $HF_MODEL_NAME"
+    optimum-cli export onnx \
+        --model $HF_MODEL_NAME \
+        --task text2text-generation-with-past \
+        --framework pt \
+        --optimize O3 \
+        $OUTPUT_DIR/$HF_MODEL_NAME;
+done;
